3c840b90c2c99792ae095c89106b00769d87a336
selcan <selcannasirli07@gmail.com>
2025-12-03 01:22:06 +0400

    Add files via upload

---
+++ p.js
@@ -0,0 +1,10 @@
+
+function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
